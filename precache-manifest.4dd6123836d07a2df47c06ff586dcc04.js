41932369c727cce9d80a0a1a37063dd30ef9551b
Ali Fata <blog.alifata@gmail.com>
2019-11-04 20:35:36 +0700

    Updates

---
+++ precache-manifest.4dd6123836d07a2df47c06ff586dcc04.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dcda39aafbb3da99d18ce53c5305760",
+    "url": "/online-shop-simple-not-finished/index.html"
+  },
+  {
+    "revision": "13f1274b9778d4fe5bcf",
+    "url": "/online-shop-simple-not-finished/static/js/2.38f4483f.chunk.js"
+  },
+  {
+    "revision": "bf226ec5fd9f1404b094",
+    "url": "/online-shop-simple-not-finished/static/js/main.3c841c43.chunk.js"
+  },
+  {
+    "revision": "6316f968660b4985641c",
+    "url": "/online-shop-simple-not-finished/static/js/runtime-main.9927703a.js"
+  }
+]);
